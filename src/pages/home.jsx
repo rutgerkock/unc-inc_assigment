162936a882018e48489ce430a35afc8ddda548b2
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2024-11-30 09:12:50 +0100

    added 3 pages
    Added Home, Dashboard, Login pages with links on the home page + links back home on the other pages.

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export function Home() {
+    return (
+        <>
+            <h1>Home</h1>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/dashboard">Dashboard</Link>
+        </>
+    )
+}
